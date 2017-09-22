a21c796f1e671c8c3bf19895b5ca6aa92a6a7c7c
filipanton <31594698+filipanton@users.noreply.github.com>
2017-09-22 22:59:03 +0200

    Dont bother
    Pixel perfect

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float PPUScale;
+    public float PPU;
+    Camera mycam;
+
+    // Use this for initialization
+    void Start()
+    {
+        mycam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        PPUScale = Screen.height / 480f;
+        mycam.orthographicSize = ((Screen.height) / (PPUScale * PPU)) * 0.5f;
+
+        if (target)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y, -10) + new Vector3(0, 10 ,0);
+        }
+    }
+}
