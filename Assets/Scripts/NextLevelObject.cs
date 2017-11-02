2e3dcbac235c3725c24ae8b57bb69ce9dfb2ddd4
filipanton <31594698+filipanton@users.noreply.github.com>
2017-11-03 00:44:24 +0100

    Menu logo thingy

---
+++ Assets/Scripts/NextLevelObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelObject : MonoBehaviour {
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
