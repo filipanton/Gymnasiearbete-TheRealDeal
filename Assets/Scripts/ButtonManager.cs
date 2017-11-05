f4248fc009489f2222de632861036e5f2ded9474
filipanton <31594698+filipanton@users.noreply.github.com>
2017-11-05 18:58:48 +0100

    :(

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    GameObject Menu_Object;
+
+    GameObject LevelSelect_Object;
+
+    public void Start()
+    {
+        Menu_Object = GameObject.Find("Menu");
+
+        LevelSelect_Object = GameObject.Find("Level Select");
+
+        LevelSelect_Object.SetActive(false);
+    }
+
+    public void LevelSelectBtn()
+    {
+
+        Menu_Object.SetActive(false);
+
+        LevelSelect_Object.SetActive(true);
+
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
