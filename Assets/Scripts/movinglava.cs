56a20371b9338c11eaf7afdb684efe0503a89dfa
filipanton <31594698+filipanton@users.noreply.github.com>
2018-03-10 20:43:21 +0100

    level 6 done

---
+++ Assets/Scripts/movinglava.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movinglava : MonoBehaviour {
+
+    public GameObject platform;
+
+    public float moveSpeed;
+
+    public Transform currentPoint;
+
+    public Transform[] points;
+
+    public int pointSelection;
+
+	void Start ()
+    {
+        currentPoint = points[pointSelection];
+
+	}
+	
+	
+	void Update ()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+	}
+}
