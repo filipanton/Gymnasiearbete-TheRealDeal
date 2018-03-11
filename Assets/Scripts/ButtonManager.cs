67b689251dab91c9f9231b11711c64add2d60c76
filipanton <31594698+filipanton@users.noreply.github.com>
2018-03-11 03:13:26 +0100

    beta

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -9,6 +9,8 @@ public class ButtonManager : MonoBehaviour {
 
     GameObject LevelSelect_Object;
 
+    GameObject Finish_Object;
+
     public void Start()
     {
         Menu_Object = GameObject.Find("Menu");
@@ -16,6 +18,8 @@ public class ButtonManager : MonoBehaviour {
         LevelSelect_Object = GameObject.Find("Level Select");
 
         LevelSelect_Object.SetActive(false);
+
+        Finish_Object = GameObject.Find("Finish");
     }
 
     public void LevelSelectBtn()
@@ -30,5 +34,40 @@ public class ButtonManager : MonoBehaviour {
     public void Level1()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Level6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Level7()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void Finish()
+    {
+        SceneManager.LoadScene(0);
     }
 }
